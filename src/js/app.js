0d20c7175f10cb777e37d73cad233babf4955531
artelbear <artelbear@gmail.com>
2018-02-17 11:17:42 +0300

    Template setup

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+// loads the Icon plugin
+UIkit.use(Icons);
+// components can be called from the imported UIkit reference
+UIkit.notification('Hello world.');
+
+$(document).ready(function () {
+    console.log('Webpack loaded!');
+});
